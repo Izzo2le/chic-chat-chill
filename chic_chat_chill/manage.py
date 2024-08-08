461d228e47b2273a3bd2293f317e33cf158f1f90
Izzo2le <isobel.otoole@aim.com>
2024-08-08 19:40:43 +0000

    updated spacing

--- chic_chat_chill/manage.py
+++ chic_chat_chill/manage.py
@@ -5,12 +5,12 @@ import sys
 
 from django.db import models
 
+from django.db import models
+
 class YourModel(models.Model):
-    title = models.CharField(max_length=200)
-     content = models.TextField()
-     created_at = models.DateTimeField(auto_now_add=True)
-      updated_at = models.DateTimeField(auto_now=True)
-     
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
 
