1f76f76a1376a7f4866257caf8ba433d17fabcbc
Izzo2le <isobel.otoole@aim.com>
2024-08-09 20:29:33 +0000

    models.py

--- chic_chat_chill/chic_chat_chill/models.py
+++ chic_chat_chill/chic_chat_chill/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class YourModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-     
-    def __str__(self):
-        return self.name
+class User(AbstractUser):
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField() 
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    bio = models.TextField(blank=True)  
