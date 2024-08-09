be82f84c917b50671feb74c08c670615fe98a04f
Izzo2le <isobel.otoole@aim.com>
2024-08-09 18:23:15 +0000

    update manage.py

--- chic_chat_chill/manage.py
+++ chic_chat_chill/manage.py
@@ -5,14 +5,7 @@ import sys
 
 from django.db import models
 
-from django.db import models
-
-class YourModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.title
+from django.db import model
 
 def main():
     """Run administrative tasks."""
