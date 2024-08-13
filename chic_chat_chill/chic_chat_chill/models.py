a231ce29c751a52f8decfd0df4f0166b91fac084
Izzo2le <isobel.otoole@aim.com>
2024-08-13 18:07:52 +0000

    models.py update

--- chic_chat_chill/chic_chat_chill/models.py
+++ chic_chat_chill/chic_chat_chill/models.py
@@ -5,6 +5,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    date_of_birth = models.DateField() 
+    date_of_birth = models.DateField()
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     bio = models.TextField(blank=True)
+
